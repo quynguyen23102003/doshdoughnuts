2afcb75ec451db226f74c3ab0f30aae75e34bd48
tanduc <ntanduc4903@gmail.com>
2023-10-13 22:44:10 +0700

    details

--- js/image.js
+++ js/image.js
@@ -1,5 +1,5 @@
 var images = [];
-var mota= [
+var mota = [
     "Say hi ! Halloween",
     "Say hi ! Halloween",
     "Nhắn Dosh Để Đặt Bánh Liền Nha",
@@ -9,23 +9,23 @@ var mota= [
 ]
 var index = 0;
 var sohinh = 6;
-for (var i=0;i<sohinh;i++) {
+for (var i = 0; i < sohinh; i++) {
     images[i] = new Image();
     images[i].src = "images/" + i + ".png";
 }
 function prev() {
     index--;
-    if(index <0) index = images.length - 1;
+    if (index < 0) index = images.length - 1;
     var anh = document.getElementById("anh");
     anh.src = images[index].src;
-    document.getElementById("mota").innerHTML= mota[index];
+    document.getElementById("mota").innerHTML = mota[index];
 }
 function next() {
     index++;
-    if(index >= images.length) index = 0;
+    if (index >= images.length) index = 0;
     var anh = document.getElementById("anh");
     anh.src = images[index].src;
-    document.getElementById("mota").innerHTML= mota[index];
+    document.getElementById("mota").innerHTML = mota[index];
 }
 
 
---
+++ js/product.js
@@ -0,0 +1,27 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const image = urlParams.get("image");
+    const title = urlParams.get("title");
+    const price = urlParams.get("price");
+
+    const productImage = document.querySelector(".detail img");
+    const productTitle = document.querySelector(".content h1");
+    const productPrice = document.querySelector(".content .price");
+
+    productImage.src = image;
+    productImage.alt = title;
+    productTitle.textContent = title;
+    productPrice.textContent += price;
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const productLinks = document.querySelectorAll(".product a");
+    productLinks.forEach(function (link) {
+        link.addEventListener("click", function (event) {
+            event.preventDefault(); // Ngăn chuyển trang mặc định
+            const productDetailURL = link.getAttribute("href");
+            window.location.href = productDetailURL;
+        });
+    });
+});
