ed7d2c29a6141d04092cce5d1bdcd553a977d286
quynguyen23102003 <quy23102003@gmail.com>
2023-10-14 22:36:35 +0700

    commit

---
+++ doshdoughtnut/js/image.js
@@ -0,0 +1,31 @@
+var images = [];
+var mota = [
+    "Say hi ! Halloween",
+    "Say hi ! Halloween",
+    "Nhắn Dosh Để Đặt Bánh Liền Nha",
+    "Order Nowww!!!",
+    "Say hi ! Halloween",
+    "Halloween coming soonnnn !",
+]
+var index = 0;
+var sohinh = 6;
+for (var i = 0; i < sohinh; i++) {
+    images[i] = new Image();
+    images[i].src = "images/" + i + ".png";
+}
+function prev() {
+    index--;
+    if (index < 0) index = images.length - 1;
+    var anh = document.getElementById("anh");
+    anh.src = images[index].src;
+    document.getElementById("mota").innerHTML = mota[index];
+}
+function next() {
+    index++;
+    if (index >= images.length) index = 0;
+    var anh = document.getElementById("anh");
+    anh.src = images[index].src;
+    document.getElementById("mota").innerHTML = mota[index];
+}
+
+setInterval("next()", 3000);
